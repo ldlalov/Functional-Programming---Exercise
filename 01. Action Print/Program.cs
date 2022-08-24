0bbceada61756903e31fd80b290bc11503281e33
Любомир Лалов <ldlalov@hotmail.com>
2022-08-24 17:21:14 +0300

    Add project files.

---
+++ 01. Action Print/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01._Action_Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            Action<string[]> Print = PrintNames;
+            Print(names);
+        }
+        static void PrintNames(string[] names)
+        {
+            foreach (string name in names)
+            {
+                Console.WriteLine($"{name}");
+            }
+        }
+    }
+}
---
+++ 02. Knights of Honor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            Action<string[]> Print = PrintNames;
+            Print(names);
+        }
+        static void PrintNames(string[] names)
+        {
+            foreach (string name in names)
+            {
+                Console.WriteLine($"Sir {name}");
+            }
+        }
+    }
+}
